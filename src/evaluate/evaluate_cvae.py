e5c89564dceb54ea789bac92195d9dfb8d8e4215
Tim-blo <79997012+Tim-blo@users.noreply.github.com>
2022-01-07 21:08:55 +0100

    Update evaluate_cvae.py

--- src/evaluate/evaluate_cvae.py
+++ src/evaluate/evaluate_cvae.py
@@ -6,15 +6,15 @@ def main():
 
     dataset = parameters["dataset"]
     print(dataset)
-    if dataset in ["ntu13pare", "ntu13", "humanact12"]:
-        print('we made it')
-        from .gru_eval import evaluate
-        evaluate(parameters, folder, checkpointname, epoch, niter)
-    elif dataset in ["uestc"]:
-        from .stgcn_eval import evaluate
-        evaluate(parameters, folder, checkpointname, epoch, niter)
-    else:
-        raise NotImplementedError("This dataset is not supported.")
+#     if dataset in ["ntu13pare", "ntu13", "humanact12"]:
+#         print('we made it')
+    from .gru_eval import evaluate
+    evaluate(parameters, folder, checkpointname, epoch, niter)
+#     elif dataset in ["uestc"]:
+#         from .stgcn_eval import evaluate
+#         evaluate(parameters, folder, checkpointname, epoch, niter)
+#     else:
+#         raise NotImplementedError("This dataset is not supported.")
 
 
 if __name__ == '__main__':
